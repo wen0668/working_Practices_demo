9339ede52de4013d07666453dda1726d75c2e47c
zhongliwen <zhongliwen0668@outlook.com>
2024-05-12 17:14:20 +0800

    增加从redis 来消费获取匹配的随机键值对的接口

--- demo2.py
+++ demo2.py
@@ -4,7 +4,7 @@ import multiprocessing
 import redis
 import random
 import json
-import string
+import datetime
 
 r = redis.Redis(host='192.168.0.4', port=6379, password='123456')
 json_stri = 'json-str.txt'
@@ -15,23 +15,52 @@ def load_model_results():
     return data_chunk
 
 data_chunk = load_model_results()
+num_keys = 1000
+num_processes = multiprocessing.cpu_count()
+keys_per_process = num_keys // num_processes
 
 app = Flask(__name__)
 queue = Queue()
-def generate_key_value_pairs(num_keys):
-    return {f'key_{i}': ''.join(random.choices(string.ascii_letters + string.digits, k=10)) for i in range(num_keys)}
 
-def feat1(data_chunk, db_index):
+def feat_1(queue, loan_id, customer_id, create_time):
+    #***feat_1处理逻辑输出dict***
+    queue.put(result)
+
+def write_to_redis1(result2, db_index):
     pipe = r.pipeline()
-    for key, value in data_chunk.items():
+    for key, value in result2.items():
         pipe.set(f'{db_index}:{key}', value)
     pipe.execute()
 
-def feat2(data_chunk, db_index):
+def write_to_redis2(result2, db_index):
     pipe = r.pipeline()
-    for key, value in data_chunk.items():
+    for key, value in result2.items():
         pipe.set(f'{db_index}:{key}', value)
     pipe.execute()
+
+def get_random_keys(r, num_keys, pattern='*'):
+    """
+    从匹配特定模式的所有键中随机选择num_keys数量的键。
+    """
+    all_keys = r.keys(pattern=pattern)
+    # 将二进制键解码为UTF-8字符串
+    all_keys_decoded = [key.decode('utf-8') for key in all_keys]
+    random_keys = random.sample(all_keys_decoded, min(num_keys, len(all_keys_decoded)))
+    return random_keys
+
+def get_keys_from_redis(queue, loan_id, customer_id, create_time):
+    random_keys = get_random_keys(r, num_keys)
+     # 使用pipeline一次性获取这些随机键的值
+    with r.pipeline() as pipe:
+        for key in random_keys:
+            pipe.get(key)
+        values = pipe.execute()
+    # 由于返回的是一个列表，我们需要将bytes类型转换为str类型
+    values = [value.decode('utf-8') if value else None for value in values]
+    # 将键和对应的值组合成一个字典
+    random_key_values = dict(zip(random_keys, values))
+    queue.put(random_key_values)
+    return random_key_values
 
 def worker(queue):
     while True:
@@ -44,16 +73,52 @@ def worker(queue):
 def index():
     data = request.get_json()
     print(data)
-    #queue.put(data)
-    num_keys = 100000
-    num_processes = multiprocessing.cpu_count()
-    keys_per_process = num_keys // num_processes
-    res = feat1(data_chunk, 'db_0')
-    res2 = feat2(data_chunk, 'db_1')
+    res = write_to_redis1(result2, 'db_0')
+    res2 = write_to_redis2(result2, 'db_1')
     queue.put(data_chunk)
     return 'Data received'
+
+@app.route("/customer", methods=['POST'])
+def predict():
+########################################################################################################################
+    req_uid = request.json['req_uid'] if 'req_uid' in request.json.keys() else None
+    loan_id = request.json['loan_id'] if 'loan_id' in request.json.keys() else None
+    customer_id = request.json['customer_id'] if 'customer_id' in request.json.keys() else None
+    create_time = request.json['create_time'] if 'create_time' in request.json.keys() else None
+    start_time = datetime.datetime.now()
+    end_time = datetime.datetime.now()
+    costSeconds = (end_time - start_time).seconds * 1.0
+
+    print("#####begin_time is ", datetime.datetime.now())
+    # 创建一个队列用于进程间通信
+    queue = Queue()
+    p1 = Process(target=get_keys_from_redis, args=(queue,loan_id, customer_id, create_time))
+    p1.start()
+    p1.join()
     
+    # 获取所有键值对
+    result_1 = get_keys_from_redis(queue,loan_id, customer_id, create_time)
+
+    result = {**result_1}
+
+    results = {
+        'req_uid': req_uid,
+        'customer_id': customer_id,
+        'create_time': create_time[0:19],
+        'code': float(0),
+        'result': result,
+        'description': 'success',
+        'costSeconds': costSeconds,
+        'finishTime': str(end_time)[0:19],
+        'collectionResultDay': str(end_time)[0:10],
+    }
+    print("end_time is ######", datetime.datetime.now())
+    return results
+
+
 if __name__ == '__main__':
-    p = Process(target=worker, args=(queue,))
-    p.start()
+    # p = Process(target=worker, args=(queue,))
+    # p.start()
+    print("所有数据已从Redis获取并合并到result2字典中")
     app.run()
+    
